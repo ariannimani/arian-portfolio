2f39e5caabac290e34ee190dedc22625445b406f
Arian N <aarriiaann@gmail.com>
2022-08-05 10:50:34 +0200

    Updated portfolio

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -5,7 +5,7 @@ const AboutMe = {
   address: "Prishtina, Kosovo",
   about: `I’m a React Developer located in Kosovo.
     Well-organised person, problem solver, independent employee with high attention to detail.    
-    Interested in the entire frontend spectrum and working on ambitious projects with positive people.`,
+    Interested in the entire frontend spectrum, blockchain, web3 and working on ambitious projects with positive people.`,
   birthDate: "1990-12-04",
   email: "ariann1990@gmail.com",
   language: ["Albanian", "English"],
@@ -13,17 +13,45 @@ const AboutMe = {
 
 const Work = [
   {
+    title: "Chat App",
+    imageUrl: "chatapp-image.jpg",
+    id: 6,
+    category: "React/Typescript/MaterialUI Project",
+    linkUrl: "https://github.com/ariannimani/chat_room_typescript",
+  },
+  {
+    title: "Crypto Dashboard",
+    imageUrl: "crypto-dashboard-image.jpg",
+    id: 5,
+    category: "React/Sass Project",
+    linkUrl: "https://github.com/ariannimani/crypto-project",
+  },
+  {
+    title: "E-commerce",
+    imageUrl: "e-commerce-image.jpg",
+    id: 4,
+    category: "React/Css Project",
+    linkUrl: "https://github.com/ariannimani/sw-ecommerce",
+  },
+  {
+    title: "E-commerce-crwn",
+    imageUrl: "ecommerce-crwn-image.jpg",
+    id: 3,
+    category: "React/Css/Sass Project",
+    linkUrl: "https://github.com/ariannimani/crwn-clothing",
+  },
+  {
     title: "MovieReact",
-    imageUrl: "project-1.jpg",
-    id: 1,
-    category: "React Project",
+    imageUrl: "cinema-image.jpg",
+    id: 2,
+    category: "React/Css Project",
     linkUrl: "https://github.com/ariannimani/cinema-react-js",
   },
   {
     title: "Portfolio",
-    imageUrl: "project-2.jpg",
-    id: 2,
-    category: "React Project",
+    imageUrl: "portfolio-image.jpg",
+    id: 1,
+    category: "React/Css Project",
     linkUrl: "https://github.com/ariannimani/ariannimani.github.io",
   },
 ];
@@ -31,33 +59,53 @@ const Work = [
 const Skills = [
   {
     id: 1,
-    language: "HTML",
-    percentage: "90%",
-  },
-  {
-    id: 2,
-    language: "CSS",
-    percentage: "70%",
-  },
-  {
-    id: 3,
-    language: "Sass",
-    percentage: "60%",
-  },
-  {
-    id: 4,
-    language: "Javascript",
-    percentage: "60%",
-  },
-  {
-    id: 5,
     language: "React",
     percentage: "70%",
   },
   {
+    id: 2,
+    language: "Javascript",
+    percentage: "60%",
+  },
+  {
+    id: 3,
+    language: "TypeScript",
+    percentage: "60%",
+  },
+  {
+    id: 4,
+    language: "Redux",
+    percentage: "40%",
+  },
+  {
+    id: 5,
+    language: "HTML",
+    percentage: "90%",
+  },
+  {
     id: 6,
+    language: "CSS",
+    percentage: "70%",
+  },
+  {
+    id: 7,
+    language: "Sass",
+    percentage: "60%",
+  },
+  {
+    id: 8,
     language: "Bootstrap",
     percentage: "50%",
+  },
+  {
+    id: 9,
+    language: "GraphQL",
+    percentage: "50%",
+  },
+  {
+    id: 10,
+    language: "T-SQL",
+    percentage: "80%",
   },
 ];
 
